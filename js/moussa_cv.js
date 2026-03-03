667dfce6dce43763b70eff21dab309bc37748e30
MoussaTraore <moutra213@gmail.com>
2026-03-03 09:49:01 +0000

    5 version

--- js/moussa_cv.js
+++ js/moussa_cv.js
@@ -1,6 +1,10 @@
 function downloadPDF(){
-    //alert("bbfbfbfbf")
+    /*alert("bbfbfbfbf")
     const moncv =document.getElementById("cv");
     html2pdf().from(moncv).save()
-    //window.print();
+    window.print();
+    */
+   //document.getElementById("msg").style.display="block"
+   alert("fichier telechargé avec succes")
+   window.print()
 }
