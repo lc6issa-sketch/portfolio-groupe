0583b4953f6a92aea4f83a25aa5c2959e058b36a
Blackdev <ibrahimfdiarra10961@gmail.com>
2026-03-05 11:20:52 +0000

    menage

--- js/scriptDiarra.js
+++ js/scriptDiarra.js
@@ -1,32 +1,15 @@
-  //
-            //   document.addEventListener('DOMContentLoaded', function() {
-            //  const downloadLink = document.getElementById('download-link');
-            //  const successMessage = document.getElementById('success-message');
+
+ document.addEventListener('DOMContentLoaded', function() {
+    const downloadLink = document.getElementById('download-link');
     
-            //  downloadLink.addEventListener('click', function(e) {
-            //  successMessage.style.display = 'flex';
-        
-            //  setTimeout(function() {
-            //   successMessage.style.animation = 'slideOut 0.3s ease';
-            //     setTimeout(function() {
-            //       successMessage.style.display = 'none';
-            //       successMessage.style.animation = ''; // Reset animation
-            //      }, 300);
-            // }, 3000);
-            // });
-            // });
-            //
-           document.addEventListener('DOMContentLoaded', function() {
-             const downloadLink = document.getElementById('download-link');
-    
-            downloadLink.addEventListener('click', function(e) {
-             e.preventDefault();
-              const link = document.createElement('a');
-              link.href = 'images/diarra.pdf';
-              link.download = 'diarra.pdf'; 
-              document.body.appendChild(link);
-              link.click();
-              document.body.removeChild(link);
-              alert('✓ Téléchargement réussi !');
-              });
-             });
+  downloadLink.addEventListener('click', function(e) {
+   e.preventDefault();
+    const link = document.createElement('a');
+    link.href = 'images/diarra.pdf';
+    link.download = 'diarra.pdf'; 
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    alert('✓ Téléchargement réussi !');
+    });
+   });
