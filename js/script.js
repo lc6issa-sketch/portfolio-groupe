e5b44bbaa82c9859ea00fcb99c3a3c899e6b7446
Idrissa Gassama <idrissag97@gmail.com>
2026-03-06 13:55:48 +0000

    modification du css de page d'accueil

--- js/script.js
+++ js/script.js
@@ -1,37 +1,13 @@
-<<<<<<< HEAD
+const cards = document.querySelectorAll(".card");
 
-document.getElementById("downloadBtn").addEventListener("click", function(e) {
-    e.preventDefault();
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add("show");
+    }
+  });
+});
 
-    // Récupérer le contenu du CV
-    const cvContent = document.documentElement.outerHTML;
-
-    // Créer un fichier Blob
-    const blob = new Blob([cvContent], { type: "text/html" });
-
-    // Créer un lien temporaire
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.download = "images/cv-maimouna.pdf";
-
-    // Déclencher le téléchargement
-    link.click();
-
-    // Libérer la mémoire
-    URL.revokeObjectURL(link.href);
-=======
-document.addEventListener("DOMContentLoaded", function () {
-
-    document.getElementById("downloadbtn").addEventListener("click", function () {
-       
-        let elt = document.querySelector(".container");
-        
-        const opt = {
-            filename: 'CV.pdf',
-            
-        };
-
-        html2pdf().set(opt).from(elt).save();
-    });
-
+cards.forEach(card => {
+  observer.observe(card);
 });
