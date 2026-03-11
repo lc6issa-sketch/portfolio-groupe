f04ddd2881c5fc4bb3dd0440b18d6910be7a1976
DOUYON <lc6issa@gmail.com>
2026-03-11 12:49:57 +0000

    Sauvegarde des changements avant de merger main

--- js/cv-douyon.js
+++ js/cv-douyon.js
@@ -1,3 +1,12 @@
-function messageCV() {
-    alert("CV téléchargé avec succès !");
-}
+const btn = document.getElementById("downloadBtn");
+const message = document.getElementById("message");
+
+btn.addEventListener("click", function(){
+
+    message.style.display = "block";
+
+    setTimeout(function(){
+        message.style.display = "none";
+    },2000);
+
+});
