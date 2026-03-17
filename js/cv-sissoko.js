67d884598da74e2c712203daf9360d87ec400878
Ibrahima Sissoko <sissokoibrahima.ml14@gmail.com>
2026-03-17 11:16:40 +0000

    modification

---
+++ js/cv-sissoko.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+             const downloadLink = document.getElementById('download-link');
+    
+            downloadLink.addEventListener('click', function(e) {
+             e.preventDefault();
+              const link = document.createElement('a');
+              link.href = 'images/sissoko.pdf';
+              link.download = 'sissoko.pdf'; 
+              document.body.appendChild(link);
+              link.click();
+              document.body.removeChild(link);
+              alert('Téléchargement éffectué avec succès !');
+              });
+             });
